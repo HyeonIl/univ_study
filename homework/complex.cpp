887c5a79ba21ad6533dc47a103edfe61db3a18d4
HyeonIl <highcho@donga.ac.kr>
2014-11-17 15:49:43 +0900

    복소수 처리부분 dmo1

--- homework/complex.cpp
+++ homework/complex.cpp
@@ -1,19 +1,13 @@
 #include<iostream>
 #include<string>
 #include<cstring>
-#include<cctype>
-
-
-
-
-//================전역 변수=======================
-
+//================전역 변수==================================================
 char oper;
 int intNum1;
 int intNum2;
 int fltNum1;
 int fltNum2;
-
+//===========================================================================
 using namespace std;
 void analyzeInput(string &input);
 int main()
@@ -22,25 +16,22 @@ int main()
 	string inputExpr;
 
 	cout << "======각 피연산자는 띄어쓰기로 구분합니다.======\n"
-
-		<< "======띄어쓰기를 올바르게 입력해 주시길바랍니다.======\n" << endl;
-
+		 << "======띄어쓰기를 올바르게 입력해 주시길바랍니다.======\n" << endl;
 	cout << "계산할 식을 입력하시오 -> ";
 
-	getline(cin, inputExpr);
-	analyzeInput(inputExpr);
+	getline(cin, inputExpr);		//입력을 한줄로 다 받아서 스트링으로 처리함
+	analyzeInput(inputExpr);		//입력받은 식을 분석함
 
 	switch (oper)
-
 	{
-	case '+':
-		break;
-	case '-':
-		break;
-	case '/':
-		break;
-	case '*':
-		break;
+		case '+':
+			break;
+		case '-':
+			break;
+		case '/':
+			break;
+		case '*':
+			break;
 	}
 
 	return 0;
@@ -49,30 +40,30 @@ int main()
 
 void analyzeInput(string &input){
 
-
-
 	const string OPERATORS = "+-*/";
-	
+	int i,operIdx = 0;
 	int midIdx = input.length() / 2;
-	
-	if (input.find("i") != string::npos){
 
-		//이것은 복소수 식이다.
+	if (input.find("i") != string::npos){				//i가 문자열에 존재한다는 것은 이식은 복소수식임
 
-		int midIdx = input.length() / 2;
+		//복소수식에서는 연산자가 총 3개 나올수 있다, 그 중 2번재 연산자의 인덱스를 찾는다.
+		for (i = 0; i < 2; i++){
+			for (int j = operIdx; j < input.length(); j++){
+				if (OPERATORS.find(input[j]) != string::npos){
+					break;
+				}
+			}
+		}
 
-		string subExpr1 = input.substr(0, midIdx - 2);				//첫번째 인덱스 ~ 연산자의 앞까지의 부분 식 즉 첫번째 복소수식
-		string subExpr2 = input.substr(midIdx + 2, input.length());	//두번째 복소수식
+		string subExpr1 = input.substr(0, operIdx - 1);				//첫번째 인덱스 ~ 연산자의 앞까지의 부분 식 즉 첫번째 복소수식
+		string subExpr2 = input.substr(operIdx + 1, input.length());	//두번째 복소수식
 
 		int imgIdx1 = subExpr1.find("i");					//첫번째 복소수 식의 i의 인덱스
 		int imgIdx2 = subExpr2.find("i");					//두번째 복소수 식의 i의 인덱스
 
 	}
 	else {
-
 		//실수 끼리의 계산 일 수도 있고 정수 끼리의 계산일 수도 있다.
-		int i;
-
 		for (i = 0; i < input.length(); i++){
 			if (OPERATORS.find(input[i]) != string::npos){
 				break;
@@ -83,7 +74,7 @@ void analyzeInput(string &input){
 		oper = input[operIdx];								//operIdx가 가진 값의 인덱스 값으로 연산자를 추출한다.
 
 		string sub1 = input.substr(0, operIdx - 1);
-		string sub2 = input.substr(operIdx + 2, input.length());
+		string sub2 = input.substr(operIdx + 1, input.length());
 
 		char *temp1 = (char*)sub1.c_str();
 		char *temp2 = (char*)sub2.c_str();
